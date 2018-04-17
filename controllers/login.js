d0954adbd586549457594840de0931bdf3892e2e
joq tyyppi <eero.ojala@helsinki.fi>
2018-04-17 19:31:33 +0300

    Added login router

---
+++ controllers/login.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+const bcryptjs = require('bcryptjs')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+const print = require('../utils/print')
+
+loginRouter.post('/', async (request, response) => {
+    try {
+        const body = request.body
+
+        const user = await User.findOne({ username: body.username })
+
+        const passwordCorrect = user === null ?
+            false :
+            await bcryptjs.compare(body.password, user.passwordHash)
+
+        if (!(user && passwordCorrect)) {
+            return response.status(401).send({ error: 'Invalid username or password' })
+        }
+
+        const userForToken = {
+            username: user.username,
+            id: user._id
+        }
+
+        const token = jwt.sign(userForToken, process.env.SECRET)
+
+        response.status(200).send({ token, username: user.username })
+    } catch (exception) {
+        print(exception)
+
+        response.status(500).send({ error: 'Something went wrong...' })
+    }
+})
