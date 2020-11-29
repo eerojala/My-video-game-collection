20fe318f6aae2b6f44db8c85e830e2082a221d75
joq tyyppi <eero.ojala@helsinki.fi>
2020-11-29 16:33:47 +0200

    Updated database link

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-}
-
-let port = process.env.NODE_ENV === 'test' ? process.env.TEST_PORT : process.env.PORT
-let mongoUrl = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
-
-module.exports = { mongoUrl, port }
+  }
+  
+  let port = process.env.PORT || 3001
+  let mongoUrl = process.env.MONGODB_URI
+  
+  module.exports = { mongoUrl, port }
